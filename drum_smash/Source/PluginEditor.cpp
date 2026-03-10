8d544ea91b16864c464a12fbd91008ca509bfc1f
Ricardo Silva <ricardos8977@gmail.com>
2026-03-10 17:53:48 +0000

    setup pages

--- drum_smash/Source/PluginEditor.cpp
+++
@@ -1,7 +0,0 @@
-#include "PluginEditor.h"
-#include "PluginProcessor.h"
-
-juce::AudioProcessorEditor* DrumSmashProcessor::createEditor()
-{
-    return new juce::GenericAudioProcessorEditor (*this);
-}
